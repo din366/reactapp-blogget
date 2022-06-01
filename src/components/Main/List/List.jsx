61eb7489689b90a21afe205121a7ce61af41b572
Alex Din <din366@gmail.com>
2022-06-01 16:22:52 +0300

    add_lesson_5.5

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,17 +2,45 @@ import style from './List.module.css';
 import Post from './Post';
 
 export const List = () => {
-  const postData = {
-    thumbnail: '',
-    // eslint-disable-next-line max-len
-    title: 'Title',
-    author: 'Nickname',
-    ups: 24,
-    date: '2022-02-24T09:45:00.000Z',
-  };
+  const postsData = [
+    {
+      thumbnail: '',
+      title: 'Title1',
+      author: 'Nickname1',
+      ups: 77,
+      date: '2022-01-21T09:45:00.000Z',
+      id: '123',
+    },
+    {
+      thumbnail: '',
+      title: 'Title2',
+      author: 'Nickname2',
+      ups: 24,
+      date: '2022-01-31T11:45:00.000Z',
+      id: '456',
+    },
+    {
+      thumbnail: '',
+      title: 'Title3',
+      author: 'Nickname3',
+      ups: 58,
+      date: '2022-03-28T09:45:00.000Z',
+      id: '678',
+    },
+    {
+      thumbnail: '',
+      title: 'Title4',
+      author: 'Nickname4',
+      ups: 39,
+      date: '2022-04-05T09:45:00.000Z',
+      id: '900',
+    }
+  ];
   return (
     <ul className={style.list}>
-      <Post postData={postData}/>
+      {postsData.map((postsData) => (
+        <Post key={postsData.id} postData={postsData}/>
+      ))}
     </ul>
   );
 };
