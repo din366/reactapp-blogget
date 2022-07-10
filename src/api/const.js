30d870b55ee46fe05cad9ab2505da7bafff19d49
Alex Din <din366@gmail.com>
2022-07-10 21:31:22 +0300

    fix const REDIRECT_ULR

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = '0oll_xySJvUTQzQpd-Oqgw';
 export const RESPONCE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://reactapp-blogget.vercel.app/auth';
 export const SCOPE = 'identity submit read';
