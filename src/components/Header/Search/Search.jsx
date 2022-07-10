6d7c3105bf9806e1bd3c1b38557f7287da774d45
Alex Din <din366@gmail.com>
2022-07-10 21:23:30 +0300

    finish lesson_5.13

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,12 +1,30 @@
 /* eslint-disable max-len */
 import style from './Search.module.css';
 import {ReactComponent as SearchImage} from './img/search.svg';
+import {useDispatch} from 'react-redux';
+import {useState} from 'react';
+import {searchRequest} from '../../../store/search/searchAction';
 
-export const Search = () => (
-  <form className={style.form}>
-    <input className={style.search} type="search" />
-    <button className={style.button}>
-      <SearchImage className={style.svg} />
-    </button>
-  </form>
-);
+export const Search = () => {
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
+
+  const handlerSubmit = e => {
+    e.preventDefault();
+    dispatch(searchRequest(search));
+  };
+
+  return (
+    <form className={style.form} onSubmit={handlerSubmit}>
+      <input
+        className={style.search}
+        type="search"
+        onChange={e => setSearch(e.target.value)}
+        value={search}
+      />
+      <button className={style.button} type='submit'>
+        <SearchImage className={style.svg} />
+      </button>
+    </form>
+  );
+};
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,12 @@ import {commentReducer} from './commentReducer';
 import {authReducer} from './auth/authReducer';
 import postsReducer from './posts/postsSlice';
 import commentsReducer from './singlePost/commentsSlice';
+import {searchReducer} from './search/searchReducer';
 import {configureStore} from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
@@ -12,7 +17,10 @@ export const store = configureStore({
     auth: authReducer,
     posts: postsReducer,
     comments: commentsReducer,
+    search: searchReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tokenMiddleware),
+    getDefaultMiddleware().concat(tokenMiddleware, sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
